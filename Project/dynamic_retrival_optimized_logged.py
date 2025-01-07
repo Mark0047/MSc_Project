b51fce537ff3438ef11c04cfb012d463aa8d0f18
Haris <hsmughal4@gmail.com>
2025-01-07 17:13:41 +0000

    debugging

--- Project/dynamic_retrival_optimized_logged.py
+++ Project/dynamic_retrival_optimized_logged.py
@@ -335,13 +335,14 @@ def handle_query(query, metadata_faiss, document_faiss, document_metadata, proce
     
     relevant_metadata = [metadata_snapshot[idx] for idx in top_indices[0] if idx < len(metadata_snapshot)]
     print('relevant_metadata-----', relevant_metadata)
-    
+    print('processed_files-----', processed_files)
     documents_to_fetch = []
     for meta in relevant_metadata:
         
-        
+       
         url = meta.get('url')
         file_name = meta.get('file_name')
+        print(url)
         if url and file_name and file_name not in processed_files:
             documents_to_fetch.append({'url': url, 'file_name': file_name})
     print('documents_to_fetch------------------------', documents_to_fetch)
