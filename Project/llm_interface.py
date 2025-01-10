d9b67f8e023f25922942ee4446c5197e691ef649
Haris <hsmughal4@gmail.com>
2025-01-10 17:04:01 +0000

    fix

--- Project/llm_interface.py
+++ Project/llm_interface.py
@@ -79,7 +79,7 @@ class HuggingFaceLLM(LLMInterface):
         self.device = device
         self._name = f"HF-{model_name}"
         try:  
-            self.pipe = pipeline(task, model=model_name,  use_auth_token= os.environ.get('hf_token')
+            self.pipe = pipeline(task, model=model_name,  use_auth_token= os.environ.get('hf_token'))
         except Exception as e:
             raise RuntimeError(f"Error loading Hugging Face model '{model_name}': {e}")
 
